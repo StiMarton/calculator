127d9b3fd6efb9e39b75451657cde8199134daa8
Stian Marton <stianmarton2@gmail.com>
2023-01-26 13:56:00 +0100

    add subtract

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const calculatorDisplay1 = document.querySelector("#display1");
 const calculatorDisplay2 = document.querySelector("#display2");
+const operatorDisplay = document.querySelector("#operator");
 const outputDisplay = document.querySelector("#output");
+
 const numBtn = document.getElementsByClassName("num-btn");
 const clearBtn = document.getElementById("clear");
 const addBtn = document.getElementById("add-btn");
+const subtractBtn = document.getElementById("subtract-btn")
 const equalBtn = document.getElementById("equal");
 
 let calculation = {
@@ -27,9 +30,11 @@ clear.addEventListener("click", function() {
     document.getElementById("output").hidden = true;
 })
 
+//The add button should call the operate function
 addBtn.addEventListener("click", function() {
     document.getElementById("operator").hidden = false;
     document.getElementById("display2").hidden = false;
+    operatorDisplay.textContent = "+";
     calculation.operator = "add";
     if (!calculation.num1) {
         calculation.num1 = calculatorDisplay1.textContent;
@@ -38,11 +43,27 @@ addBtn.addEventListener("click", function() {
     }
 })
 
+subtractBtn.addEventListener("click", function() {
+    document.getElementById("operator").hidden = false;
+    document.getElementById("display2").hidden = false;
+    operatorDisplay.textContent = "-";
+    calculation.operator = "subtract";
+    if (!calculation.num1) {
+        calculation.num1 = calculatorDisplay1.textContent;
+        console.log("Subtract 1 works");
+    } else if (!calculation.num2) {
+        calculation.num2 = calculatorDisplay2.textContent;
+        console.log("Subtract 2 works");
+    }
+})
+
 equalBtn.addEventListener("click", function() {
     document.getElementById("output").hidden = false;
     console.log(calculation.num1, calculation.num2);
     if (calculation.operator == "add") {
         add(calculation.num1, calculation.num2)
+    } else if (calculation.operator == "subtract") {
+        subtract(calculation.num1, calculation.num2)
     }
 })
 
@@ -55,7 +76,11 @@ function add (num1, num2) {
 }
 
 function subtract (num1, num2) {
-    return num1 - num2;
+    num1 = Number(calculation.num1);
+    num2 = Number(calculation.num2);
+    calculation.output = num1 - num2;
+    outputDisplay.textContent = calculation.output;
+    console.log(num1, num2);
 }
 
 function multiply (num1, num2) {
@@ -72,7 +97,7 @@ function operate (operator, num1, num2) {
 
 for (let i = 0; i < numBtn.length; i++) {
     numBtn[i].addEventListener("click", function() {
-    if (calculation.operator == "add") {
+    if (calculation.operator == "add" || calculation.operator == "subtract") {
         num2 = numBtn[i].value;
         calculatorDisplay2.textContent += num2;
         calculation.num2 = calculatorDisplay2.textContent ;
