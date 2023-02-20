bd79ef0dee46d6724a1995f6ff62dabccf2e3379
Stian Marton <stianmarton2@gmail.com>
2023-02-20 19:10:33 +0100

    Added fonts, decimal working (one bug)

--- script.js
+++ script.js
@@ -17,22 +17,21 @@ let calculation = {
     num2: null,
 };
 
-let value;
 
-calculatorDisplay.textContent = value;
-calculatorDisplay2.textContent = value;
+calculatorDisplay.textContent = calculation.num1;
+calculatorDisplay2.textContent = calculation.num2;
 
 clear.addEventListener("click", function() {
     calculation.num1 = null;
     calculation.num2 = null;
     operator = null;
-    value = null;
-    calculatorDisplay.textContent = value;
-    calculatorDisplay2.textContent = value;
+    calculatorDisplay.textContent = calculation.num1;
+    calculatorDisplay2.textContent = calculation.num2;
     operatorDisplay.textContent = operator;
     document.getElementById("operator").hidden = true;
     document.getElementById("display2").hidden = true;
     equalsSelected = false;
+    enableDotBtn();
 });
 
 dotBtn.addEventListener("click", function() {
@@ -40,11 +39,24 @@ dotBtn.addEventListener("click", function() {
         calculation.num1 = "0.";
     } else if (calculation.num1 && !operator && !calculation.num2) {
         calculation.num1 = calculation.num1 + ".";
-        console.log("dot activated, num1 value is " + calculation.num1);
+    } else if (operator && !calculation.num2) {
+        calculation.num2 = "0.";
+    } else if (calculation.num2) {
+        calculation.num2 = calculation.num2 + ".";
     }
-    console.log(calculation.num1);
     calculatorDisplay.textContent = calculation.num1;
+    calculatorDisplay2.textContent = calculation.num2;
+    console.log(result);
+    disableDotBtn();
 });
+
+function disableDotBtn() {
+    dotBtn.disabled = true;
+}
+
+function enableDotBtn() {
+    dotBtn.disabled = false;
+}
 
 for (let i = 0; i < numBtn.length; i++) {
     numBtn[i].addEventListener("click", function() {
@@ -55,17 +67,19 @@ for (let i = 0; i < numBtn.length; i++) {
             calculation.num1 = value;
             equalsSelected = false;
         } else if (!calculation.num1 || !operator) {
-        for (var num in calculation)
-            operator = "";
-            operatorDisplay.textContent = operator.textContent;
-            num = numBtn[i].value;
-            calculatorDisplay.textContent += num;
-            value = calculatorDisplay.textContent;
-    } else {
-        for (var num in calculation)
-            num = numBtn[i].value;
-            calculatorDisplay2.textContent += num;
-            value = calculatorDisplay2.textContent;
+            for (var num in calculation)
+                operator = "";
+                operatorDisplay.textContent = operator.textContent;
+                num = numBtn[i].value;
+                calculatorDisplay.textContent += num;
+                value = calculatorDisplay.textContent;
+                calculation.num1 = value;
+        } else {
+            for (var num in calculation)
+                num = numBtn[i].value;
+                calculatorDisplay2.textContent += num;
+                value = calculatorDisplay2.textContent;
+                calculation.num2 = value;
         }
     });
 };
@@ -81,21 +95,12 @@ operators.forEach(o => {
                 calculation.num1 = operate(operator, calculation.num1, calculation.num2);
                 calculation.num2 = "";
             }
-        } 
-        if (!calculation.num1) {
-            calculation.num1 = value;
-        } else {
-            calculation.num2 = value;
-          
         }
-        console.log(calculation);
-        console.log(operator);
+        enableDotBtn();
         equalsSelected = false;
         return operator;
     });
 });
-
-
 
 equalBtn.addEventListener("click", function() {
     calculation.num2 = value;
@@ -107,15 +112,8 @@ equalBtn.addEventListener("click", function() {
         calculatorDisplay.textContent = result;
         calculation.num2 = 0;
         calculatorDisplay2.textContent = "";
-        console.log("Equal 1 is " + result);
-    } else if (calculation.num1 && operator) {
-        result = operate(operator, calculation.num1, calculation.num2)
-        calculation.num1 = result;
-        calculatorDisplay.textContent = result;
-        console.log("Equal 2 works");
-    }
-    console.log("The value of number two is " + calculation.num2);
-    console.log("The operator is " + operator);
+        enableDotBtn();
+    } 
     equalsSelected = true;
 });
 
@@ -127,7 +125,7 @@ function operate(operator, num1, num2) {
         case '-':
             operator = "";
             return subtract (num1, num2);
-        case 'x':
+        case '*':
             operator = "";
             return multiply (num1, num2);
         case '÷':
@@ -136,18 +134,16 @@ function operate(operator, num1, num2) {
     }
 }
 
-// CALCULATION FUNCTIONS
-
 function add(num1, num2) {
     return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return Math.round(num1 - num2 * 100) / 100;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return Math.round(num1 * num2 * 100) / 100;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
